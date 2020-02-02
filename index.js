d7fdbc405669380816c5a5a235b06d7fcd60f589
prrdee <18732242+prrdee@users.noreply.github.com>
2020-02-02 14:57:38 +0200

    add schema and use it to populate database

--- index.js
+++ index.js
@@ -1,86 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const cors = require('cors')
-const mongoose = require('mongoose')
+const cors = require("cors");
+const mongoose = require("mongoose");
+const Observation = require("./models/observation");
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 //middlewares created by me
 const requestLogger = (request, response, next) => {
-    console.log("Method:", request.method);
-    console.log("Path:  ", request.path);
-    console.log("Body:  ", request.body);
-    console.log("---");
-    next();
-  };
-  
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
 app.use(requestLogger);
 
-const url = "mongodb+srv://fullstack_pradeep:fullstackpradeep@cluster0-uo4sj.mongodb.net/birdwatch-app?retryWrites=true&w=majority"
-
-mongoose.connect(url, {useNewUrlParser: true})
-
-const observationSchema = new mongoose.Schema({
-  name: String,
-  rarity: String,
-  notes: String,
-  timestamp: Date
-})
-
-const Observation = mongoose.model('Observation', observationSchema)
-
-const observation = new Observation({
-  name: 'Sparrow',
-  rarity: 'Common',
-  notes: 'active during day',
-  timestamp: new Date(),
-})
-
-observation.save().then(response => {
-  console.log('observation saved!')
-  mongoose.connection.close()
-})
-
-
-let observations = [
-  {
-    id: 1,
-    name: "Peakcock",
-    rarity: "common",
-    notes: "visibale only on day",
-    timestamp: "2019-05-30T17:30:31.098Z"
-  },
-  {
-    id: 2,
-    name: "Sparrow",
-    rarity: "rare",
-    notes: "visibale only on day",
-    timestamp: "2019-05-30T17:30:31.098Z"
-  },
-  {
-    id: 3,
-    name: "Crow",
-    rarity: "rare",
-    notes: "visibale only on day",
-    timestamp: "2019-05-30T17:30:31.098Z"
-  },
-  {
-    id: 4,
-    name: "Kandre",
-    rarity: "common",
-    notes: "visibale only on day",
-    timestamp: "2019-05-30T17:30:31.098Z"
-  },
-  {
-    id: 5,
-    name: "Blabla",
-    rarity: "extremely rare",
-    notes: "visibale only on day",
-    timestamp: "2019-05-30T17:30:31.098Z"
-  }
-];
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
@@ -88,67 +27,49 @@ app.get("/", (req, res) => {
 
 // get all the observations
 app.get("/api/observations", (req, res) => {
-  res.json(observations);
+  Observation.find({}).then(observations => {
+    response.json(observations.map(observation => Observation.toJSON()))
+  })
 });
 
 // get a single observsation
 app.get("/api/observations/:id", (request, response) => {
-  const id = Number(request.params.id);
-  //console.log(id)
-  const observation = observations.find(observation => observation.id === id);
-  //console.log(observation)
-  if (observation) {
-    response.json(observation);
-  } else {
-    response.status(404).end();
-  }
+  Observation.findById(request.params.id).then(observation => {
+    response.json(observation.toJSON());
+  });
 });
-// implementation of deleting single observation is not on requirement
 
-//function for generating an id to new observation
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
-  return maxId + 1;
-};
+// implementation of deleting single observation is not on requirement
 
 // post a single observation
 app.post("/api/observations", (request, response) => {
   const body = request.body;
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: "name missing"
-    });
+  if (body.name === undefined) {
+    return response.status(400).json({ error: "name missing" });
   }
 
-  const observation = {
+  const observation = new Observation({
     name: body.name,
     rarity: body.rarity,
-    // if notes is missing default value will read empty
-    notes: body.notes || "empty",
-    timestamp: new Date(),
-    id: generateId()
-  };
+    notes: body.notes,
+    timestamp: new Date()
+  });
 
-  observations = observations.concat(observation);
-
-  response.json(observation);
+  observation.save().then(savedObservation => {
+    response.json(savedObservation.toJSON);
+  });
 });
 
-
-
 //using the middlewares created above
-
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 
-
 app.use(unknownEndpoint);
 
-
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);

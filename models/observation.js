6ceab53c1d52e9ce41a4ffce9f2383304c0602aa
prrdee <18732242+prrdee@users.noreply.github.com>
2020-02-02 17:12:33 +0200

    remove __v and change _id to id

--- models/observation.js
+++ models/observation.js
@@ -1,32 +1,34 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 mongoose.set('useFindAndModify', false)
 
+const url = process.env.MONGODB_URI;
 
-const url = process.env.MONGODB_URI
+console.log("connecting to", url);
 
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true} )
-.then(result => {
-    console.log('connected to MongoDB')
-}).catch((error)=>{
-    console.log('error connecting to MongoDB: ',error.message)
-})
+mongoose
+  .connect(url, { useNewUrlParser: true })
+  .then(result => {
+    console.log("connected to MongoDB");
+  })
+  .catch(error => {
+    console.log("error connecting to MongoDB: ", error.message);
+  });
 
 const observationSchema = new mongoose.Schema({
-    name: String,
-    rarity: String,
-    notes: String,
-    timestamp: Date
-})
+  name: String,
+  rarity: String,
+  notes: String,
+  timestamp: Date
+});
 
-observationSchema.set('toJSON', {
-    transfrom: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__V
-    }
-})
 
-module.exports = mongoose.model('Observation', observationSchema)
+observationSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+module.exports = mongoose.model("Observation", observationSchema);
